e6f489b96de317730a1f9c4962312b601de11ef7
João André <joaocandre@gmail.com>
2023-10-10 17:26:39 +0100

    Add initial sources and package metadata

---
+++ src/rosaria2node.cpp
@@ -0,0 +1,7 @@
+#include <string> 
+#include "rosaria2/rosaria2node.hpp"
+
+RosAria2Node::RosAria2Node(const std::string& name) :
+    rclcpp::Node(name) {
+        /* ... */
+}
---
+++ src/test.cpp
@@ -0,0 +1,9 @@
+
+# include "rosaria2/rosaria2node.hpp"
+
+int main(int argc, char const *argv[]) {
+
+    auto node = RosAria2Node();
+
+    return 0;
+}
